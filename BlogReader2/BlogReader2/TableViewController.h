2f84aeb043e68943cd135808d37caf26ce7fd36e
Dean Laurea <deanlaurea@icloud.com>
2015-07-23 13:58:09 -0500

    Create BlogReader2

---
+++ BlogReader2/BlogReader2/TableViewController.h
@@ -0,0 +1,15 @@
+//
+//  TableViewController.h
+//  BlogReader2
+//
+//  Created by Dean Laurea on 7/23/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@property (strong, nonatomic) NSArray *titles;
+
+@end
